4a9d535b1f1d48b74653d0893479938c7570ac56
My95dn <doanvanmy.1995@gmail.com>
2023-11-09 18:55:47 +0700

    appchat

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 
-let sever = io("https://chat-my95dn.vercel.app/" || "https://chat-git-master-my95dn.vercel.app" || "https://chat-eight-wine.vercel.app")
+let sever = io("https://chat-my95dn.vercel.app/")
 
 
 let int = document.querySelector('.int')
